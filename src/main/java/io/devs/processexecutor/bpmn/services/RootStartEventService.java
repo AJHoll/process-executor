ab1f56992cd07fcf1700e9815820f2d4c4dca57c
AJHoll <anthony_j_holl@mail.ru>
2023-02-09 20:34:36 +0300

    include JOOQ in project

--- src/main/java/io/devs/processexecutor/bpmn/services/RootStartEventService.java
+++ src/main/java/io/devs/processexecutor/bpmn/services/RootStartEventService.java
@@ -1,17 +1,26 @@
 package io.devs.processexecutor.bpmn.services;
 
+import com.tej.JooQDemo.jooq.sample.model.Tables;
+import com.tej.JooQDemo.jooq.sample.model.tables.ActRuExecution;
 import io.devs.processexecutor.bpmn.interfaces.BpmExecutionStart;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.jooq.DSLContext;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Log4j2
 @Service
 @RequiredArgsConstructor
 public class RootStartEventService implements BpmExecutionStart {
+
+    private final DSLContext dsl;
     @Override
     public void start(DelegateExecution execution) {
         log.info("Process " + execution.getBusinessKey() + " started!");
+        List<ActRuExecution> exec = dsl.selectFrom(Tables.ACT_RU_EXECUTION).fetchInto(ActRuExecution.class);
+
     }
 }
